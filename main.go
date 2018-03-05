eab17c37712aa386f18b74853e83bc955634e885
Denis Grigor <apprentice3d@gmail.com>
2018-03-05 11:50:03 -0500

    fixed wrong merging.

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/apprentice3d/forge-api-go-client/recap"
+	"sync"
 )
 
 func main() {
@@ -37,10 +38,10 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	recapAPI := recap.NewReCapAPIWithCredentials(clientID, clientSecret)
+	recapAPI := recap.NewAPIWithCredentials(clientID, clientSecret)
 
 	fmt.Println("Creating a scene ...")
-	scene, err := recapAPI.CreatePhotoScene("example", []string{"obj"})
+	scene, err := recapAPI.CreatePhotoScene("example", []string{"obj", "rcm"}, "object")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -48,14 +49,45 @@ func main() {
 	fmt.Printf("Scene created: id = %s\n", scene.ID)
 
 	fmt.Println("Uploading sample images ...standby...")
-	uploadResults, err := recapAPI.AddFilesToScene(&scene, images)
-	if err != nil {
-		log.Fatal(err.Error())
+	var wg sync.WaitGroup
+	wg.Add(len(images))
+	for idx, filename := range images {
+		go func(idx int, filename string) {
+			defer wg.Done()
+			status := fmt.Sprintf("[%2d/%d] File %s ", idx+1, len(images), filename)
+			file, err := os.Open(filename)
+			if err != nil {
+				status += "failed to upload: " + err.Error()
+				log.Println(status)
+				return
+			}
+			defer file.Close()
+			data, err := ioutil.ReadAll(file)
+			if err != nil {
+				status += "failed to upload: " + err.Error()
+				log.Println(status)
+				return
+			}
+			_, err = recapAPI.AddFileToSceneUsingData(scene.ID, data)
+			if err != nil {
+				status += "failed to upload: " + err.Error()
+				log.Println(status)
+				return
+			}
+			status += "uploaded successfully"
+			log.Println(status)
+
+		}(idx, filename)
 	}
+	<<<<<<< HEAD
 	fmt.Printf("%d files where uploaded successfully\n", len(uploadResults))
+	=======
+
+	wg.Wait()
+	>>>>>>> master
 
 	fmt.Println("Starting scene processing ...")
-	if _, err = recapAPI.StartSceneProcessing(scene); err != nil {
+	if _, err = recapAPI.StartSceneProcessing(scene.ID); err != nil {
 		log.Println(err.Error())
 	}
 
@@ -63,7 +95,7 @@ func main() {
 	var progressResult recap.SceneProgressReply
 	var ratio float64
 	for {
-		if progressResult, err = recapAPI.GetSceneProgress(scene); err != nil {
+		if progressResult, err = recapAPI.GetSceneProgress(scene.ID); err != nil {
 			log.Printf("Failed to get the PhotoScene progress: %s\n", err.Error())
 			return
 		}
@@ -82,75 +114,98 @@ func main() {
 	}
 
 	fmt.Println("\nFinished processing the scene, now getting the results in obj format...")
-	result, err := recapAPI.GetSceneResults(scene, "obj")
+	result, err := recapAPI.GetSceneResults(scene.ID, "obj")
 	if err != nil {
 		log.Println(err.Error())
 	}
 
 	fmt.Printf("Results are available at following link => %s\n", result.PhotoScene.SceneLink)
-	if err = downloadLink(result.PhotoScene.SceneLink); err != nil {
+	if err := downloadLink(result.PhotoScene.SceneLink, "result_obj.zip"); err != nil {
 		log.Println("WARNING: Could not download the provided link")
 	} else {
 		workDir, _ := os.Getwd()
-		fmt.Printf("File downloaded to %s as 'result.zip'\n", workDir)
+		fmt.Printf("File downloaded to %s as 'result_obj.zip'\n", workDir)
 	}
 
-	fmt.Println("Deleting the scene ...")
-	_, err = recapAPI.DeleteScene(scene)
+
+	fmt.Println("\nNow downloading the results in rcm format...")
+	result, err = recapAPI.GetSceneResults(scene.ID, "rcm")
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Println(err.Error())
 	}
-	fmt.Println("Scene deleted successfully!")
-}
 
-func downloadLink(link string) (err error) {
+	fmt.Printf("Results are available at following link => %s\n", result.PhotoScene.SceneLink)
+	<<<<<<< HEAD
+	if err = downloadLink(result.PhotoScene.SceneLink); err != nil {
+		=======
+		if err := downloadLink(result.PhotoScene.SceneLink, "result_rcm.zip"); err != nil {
+			>>>>>>> master
+			log.Println("WARNING: Could not download the provided link")
+		} else {
+			workDir, _ := os.Getwd()
+			fmt.Printf("File downloaded to %s as 'result_rcm.zip'\n", workDir)
+		}
+
+		fmt.Println("Deleting the scene ...")
+		_, err = recapAPI.DeleteScene(scene.ID)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Println("Scene deleted successfully!")
+	}
+
+	func downloadLink(link, filename string) (err error) {
 	resp, err := http.Get(link)
 	if err != nil {
-		return
+	return
 	}
 	defer resp.Body.Close()
+	<<<<<<< HEAD
 
 	result, err := os.Create("result.zip")
+	=======
+	result, err := os.Create(filename)
+	>>>>>>> master
 	if err != nil {
-		return
+	return
 	}
 	defer result.Close()
 
 	_, err = io.Copy(result, resp.Body)
 
 	return
-}
+	}
 
-func getListOfJPGFilesFromPath(dir string) (images []string, err error) {
+	func getListOfJPGFilesFromPath(dir string) (images []string, err error) {
 	files, err := ioutil.ReadDir(dir)
 
 	if err != nil {
-		return nil, err
+	return nil, err
 	}
 
 	for _, file := range files {
-		if !file.IsDir() {
-			if strings.Compare(strings.ToLower(filepath.Ext(file.Name())), ".jpg") == 0 {
-				images = append(images, filepath.Join(dir, file.Name()))
-			}
-		}
+	if !file.IsDir() {
+	if strings.Compare(strings.ToLower(filepath.Ext(file.Name())), ".jpg") == 0 {
+	images = append(images, filepath.Join(dir, file.Name()))
+	}
+	}
 	}
 
 	if len(images) == 0 {
-		err = errors.New("no valid images found for upload")
+	err = errors.New("no valid images found for upload")
 	}
 
 	return
-}
+	}
 
-func getCredentials() (clientID string, clientSecret string, err error) {
+	func getCredentials() (clientID string, clientSecret string, err error) {
 	clientID = os.Getenv("FORGE_CLIENT_ID")
 	clientSecret = os.Getenv("FORGE_CLIENT_SECRET")
 
 	if len(clientID) == 0 || len(clientSecret) == 0 {
-		err = errors.New("\nFORGE_CLIENT_ID and FORGE_CLIENT_SECRET env vars could not be found.\n" +
-			"We encourage using Forge secrets by specifying them as env variables.\nExiting ...")
+	err = errors.New("\nFORGE_CLIENT_ID and FORGE_CLIENT_SECRET env vars could not be found.\n" +
+	"We encourage using Forge secrets by specifying them as env variables.\nExiting ...")
 	}
 
 	return
-}
+	}
